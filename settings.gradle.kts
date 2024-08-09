6c47f8bbd4295f4173819a6f8e315b51a3cd467d
Kaue Gatto <kauegatto123@gmail.com>
2024-08-09 15:22:48 -0300

    feat: add subproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,12 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "cards-scratch"
 
 val modules: List<String> = listOf(
     "cards-domain",
     "cards-gameserver",
+    "cards-networking"
 )
 
 include(modules)
