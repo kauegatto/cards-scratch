fb33ce4cd3394927d50b9173cefd987f7c79d165
Kaue Gatto <kauegatto123@gmail.com>
2024-08-09 15:16:17 -0300

    woopsie doopsy

---
+++ cards-domain/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
